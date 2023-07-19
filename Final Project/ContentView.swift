feb8eb89bac8d8a10df8b87fa296f71d522e33bf
ak9391 <ak9391@nyu.edu>
2023-07-19 19:30:11 +0530

    Initial Commit

---
+++ Final Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Final Project
+//
+//  Created by Ashrita Kantamneni on 7/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Ashrita Kantamneni on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
